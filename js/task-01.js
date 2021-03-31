fcf2ac335a4fbd011eddbb7007fd71f3299b9c8a
TarasovDR <d.r.tarasov03@gmail.com>
2021-03-31 16:40:25 +0300

    task 1 done

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const itemsNumberRef = document.querySelectorAll('.item');
+console.log(`В списке ${itemsNumberRef.length} категории.`);
+
+for (let i = 0; i < itemsNumberRef.length; i += 1) {
+  const element = itemsNumberRef[i].querySelector('h2');
+  const elementNumber = itemsNumberRef[i].querySelectorAll('ul > li');
+
+  console.log(`Категория: ${element.textContent}`);
+  console.log(`Количество элементов: ${elementNumber.length}`);
+}
